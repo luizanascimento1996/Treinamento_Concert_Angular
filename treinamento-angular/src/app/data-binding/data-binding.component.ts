82e37726c14749e281deca85ad848a2691bc30af
Luiza Nascimento e Silva <45200096+luizanascimento1996@users.noreply.github.com>
2020-11-11 14:32:48 -0300

    Bindings
    Aulas sobre class, style, event e property binding.

--- treinamento-angular/src/app/data-binding/data-binding.component.ts
+++ treinamento-angular/src/app/data-binding/data-binding.component.ts
@@ -7,8 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataBindingComponent implements OnInit {
   nome: string = 'Luiza Nascimento e Silva';
+  cursoAngular = true;
+  urlImage =
+    'https://i.picsum.photos/id/980/400/200.jpg?hmac=9-zpSqiyyseLFp_GvE-Ayrh7oEPif4JaKd42TbczZ10';
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  getValor() {
+    return 1;
+  }
+
+  getCurtirCurso() {
+    return true;
+  }
+
+  botaoClicado() {
+    alert('Clicou');
+  }
 }
