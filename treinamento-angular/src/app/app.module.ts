b8b7337ea80bb1afddd9181bcc1f342f89a3ff58
Luiza Nascimento e Silva <45200096+luizanascimento1996@users.noreply.github.com>
2020-11-16 11:30:30 -0300

    Criando um serviço e Arrumando as pastas do projeto

--- treinamento-angular/src/app/app.module.ts
+++ treinamento-angular/src/app/app.module.ts
@@ -4,26 +4,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MeuPrimeiroComponent } from './meu-primeiro-component/meu-primeiro.component';
-import { MeuPrimeiroComponentCliComponent } from './meu-primeiro-component-cli/meu-primeiro-component-cli.component';
-import { CursosModule } from './cursos/cursos.module';
-import { DataBindingComponent } from './data-binding/data-binding.component';
+import { MeuPrimeiroComponent } from './Funcionamento/meu-primeiro-component/meu-primeiro.component';
+import { MeuPrimeiroComponentCliComponent } from './Funcionamento/meu-primeiro-component-cli/meu-primeiro-component-cli.component';
+import { CursosModule } from './Funcionamento/cursos/cursos.module';
+import { DataBindingComponent } from './Funcionamento/data-binding/data-binding.component';
 import { FormsModule } from '@angular/forms';
-import { InputPropertyComponent } from './input-property/input-property.component';
-import { OutputPropertyComponent } from './output-property/output-property.component';
-import { CicloComponent } from './ciclo/ciclo.component';
-import { DiretivaIfComponent } from './diretiva-if/diretiva-if.component';
-import { DiretivaSwitchComponent } from './diretiva-switch/diretiva-switch.component';
-import { DiretivaForComponent } from './diretiva-for/diretiva-for.component';
-import { DiretivaNgclassComponent } from './diretiva-ngclass/diretiva-ngclass.component';
-import { DiretivaNgstyleComponent } from './diretiva-ngstyle/diretiva-ngstyle.component';
-import { OperadorElvisComponent } from './operador-elvis/operador-elvis.component';
-import { ExemploNgcontentComponent } from './exemplo-ngcontent/exemplo-ngcontent.component';
+import { InputPropertyComponent } from './Funcionamento/input-property/input-property.component';
+import { OutputPropertyComponent } from './Funcionamento/output-property/output-property.component';
+import { CicloComponent } from './Funcionamento/ciclo/ciclo.component';
+import { DiretivaIfComponent } from './Diretivas/diretiva-if/diretiva-if.component';
+import { DiretivaSwitchComponent } from './Diretivas/diretiva-switch/diretiva-switch.component';
+import { DiretivaForComponent } from './Diretivas/diretiva-for/diretiva-for.component';
+import { DiretivaNgclassComponent } from './Diretivas/diretiva-ngclass/diretiva-ngclass.component';
+import { DiretivaNgstyleComponent } from './Diretivas/diretiva-ngstyle/diretiva-ngstyle.component';
+import { OperadorElvisComponent } from './Funcionamento/operador-elvis/operador-elvis.component';
+import { ExemploNgcontentComponent } from './Funcionamento/exemplo-ngcontent/exemplo-ngcontent.component';
 import { FundoAmareloDirective } from './shared/fundo-amarelo.directive';
-import { DiretivasCustomizadasComponent } from './diretivas-customizadas/diretivas-customizadas.component';
+import { DiretivasCustomizadasComponent } from './Diretivas/diretivas-customizadas/diretivas-customizadas.component';
 import { HighlightMouseDirective } from './shared/highlight-mouse.directive';
 import { HighlightDirective } from './shared/highlight.directive';
 import { NgElseDirective } from './shared/ng-else.directive';
+import { CursosComponent } from './Servicos/cursos/cursos.component';
 
 @NgModule({
   declarations: [
@@ -46,6 +47,7 @@ import { NgElseDirective } from './shared/ng-else.directive';
     HighlightMouseDirective,
     HighlightDirective,
     NgElseDirective,
+    CursosComponent,
   ],
   imports: [
     BrowserModule,
