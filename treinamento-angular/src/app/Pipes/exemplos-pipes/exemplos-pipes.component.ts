f1bbde372b897f1c7d6d8e071403ffef3b12f2ff
Luiza Nascimento e Silva <45200096+luizanascimento1996@users.noreply.github.com>
2020-11-17 09:59:11 -0300

    Pipes: Async

--- treinamento-angular/src/app/Pipes/exemplos-pipes/exemplos-pipes.component.ts
+++ treinamento-angular/src/app/Pipes/exemplos-pipes/exemplos-pipes.component.ts
@@ -16,7 +16,11 @@ export class ExemplosPipesComponent implements OnInit {
       'https://www.amazon.com.br/Learning-JavaScript-Data-Structures-Algorithms-ebook/dp/B077NB5H6Y/ref=sr_1_2?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=loiane+groner&qid=1605546229&sr=8-2',
   };
 
-  livros: string[] = ['Angular', 'JAVA' ];
+  livros: string[] = ['Angular', 'JAVA'];
+
+  valorAsync = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('Valor Assíncrono'), 2000)
+  });
 
   constructor() {}
 
