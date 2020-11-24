76373e8bbe38fec43716bc4eee284ed724c93b2e
Luiza Nascimento e Silva <45200096+luizanascimento1996@users.noreply.github.com>
2020-11-24 18:45:51 -0300

    treinamento csharp - Aula 3 (Funcionando)

---
+++ treinamento-csharp2/VeiculosAPI/Migrations/20201124200859_InitialCreate.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace VeiculosAPI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Caminhao",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    cor = table.Column<string>(type: "text", nullable: true),
+                    placa = table.Column<string>(type: "text", nullable: true),
+                    marca = table.Column<string>(type: "text", nullable: true),
+                    modelo = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Caminhao", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Carro",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    cor = table.Column<string>(type: "text", nullable: true),
+                    placa = table.Column<string>(type: "text", nullable: true),
+                    marca = table.Column<string>(type: "text", nullable: true),
+                    modelo = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carro", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Moto",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    cor = table.Column<string>(type: "text", nullable: true),
+                    placa = table.Column<string>(type: "text", nullable: true),
+                    marca = table.Column<string>(type: "text", nullable: true),
+                    modelo = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Moto", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Caminhao");
+
+            migrationBuilder.DropTable(
+                name: "Carro");
+
+            migrationBuilder.DropTable(
+                name: "Moto");
+        }
+    }
+}
